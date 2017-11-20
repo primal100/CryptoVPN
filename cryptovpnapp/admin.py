b1592e6797aeb19f728b9d2f5e91eb17f98b34f7
primal100 <paul>
2017-11-20 12:13:00 +0000

    Added log entries toa admin

--- cryptovpnapp/admin.py
+++ cryptovpnapp/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from django.contrib.admin.models import LogEntry, DELETION
+from django.utils.html import escape
+from django.core.urlresolvers import reverse
 from django.contrib.auth.models import Group
 from .models import User, Service, SubscriptionType, Subscription, Address, Invoice, Transaction, RefundRequest, Comment
 
@@ -29,6 +32,59 @@ class ReadOnlyAdmin(admin.ModelAdmin):
     def save_related(self, request, form, formsets, change):
         pass
 
+
+class LogEntryAdmin(admin.ModelAdmin):
+    date_hierarchy = 'action_time'
+
+    readonly_fields =  [f.name for f in LogEntry._meta.get_fields()]
+
+    list_filter = [
+        'user',
+        'content_type',
+        'action_flag'
+    ]
+
+    search_fields = [
+        'object_repr',
+        'change_message'
+    ]
+
+    list_display = [
+        'action_time',
+        'user',
+        'content_type',
+        'object_link',
+        'action_flag',
+        'change_message',
+    ]
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return request.user.is_superuser and request.method != 'POST'
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def object_link(self, obj):
+        if obj.action_flag == DELETION:
+            link = escape(obj.object_repr)
+        else:
+            ct = obj.content_type
+            link = u'<a href="%s">%s</a>' % (
+                reverse('admin:%s_%s_change' % (ct.app_label, ct.model), args=[obj.object_id]),
+                escape(obj.object_repr),
+            )
+        return link
+
+    object_link.allow_tags = True
+    object_link.admin_order_field = 'object_repr'
+    object_link.short_description = u'object'
+
+    def queryset(self, request):
+        return super(LogEntryAdmin, self).queryset(request) \
+            .prefetch_related('content_type')
 
 class UserAdmin(admin.ModelAdmin):
     list_display = ('username', 'email', 'date_joined', 'last_login', 'is_staff', 'is_superuser', 'is_active', )
@@ -67,4 +123,5 @@ admin.site.register(Invoice, InvoiceAdmin)
 admin.site.register(Transaction, TransactionAdmin)
 admin.site.register(RefundRequest, RefundRequestAdmin)
 admin.site.register(Comment, CommentAdmin)
+admin.site.register(LogEntry, LogEntryAdmin)
 admin.site.unregister(Group)
